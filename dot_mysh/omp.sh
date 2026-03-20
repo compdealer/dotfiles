d8bbfd8108d67c6576a48b11301d746b0d4e4928
Владислав Павлов <doctorpavlov@gmail.com>
2026-03-20 14:11:32 +0300

    Update .mysh/omp.sh

--- dot_mysh/omp.sh
+++ dot_mysh/omp.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 apt-get install unzip
 curl -s https://ohmyposh.dev/install.sh | bash -s
-echo 'eval "$(oh-my-posh init bash)"' >> ~/.bashrc
+
+echo 'eval "$(oh-my-posh init bash --config powerlevel10k_rainbow)"' >> ~/.bashrc
